6a93db06fb1ebc9c6ce8a8f444d71d8230dbb422
Jonathan Gabel <post@jonathangabel.com>
2012-10-02 17:33:22 -0400

    ready to integrate fly.color

--- hexcolor.js
+++ hexcolor.js
@@ -3,6 +3,21 @@
 var fly = {};
 
 fly.color = (function() {
+// version 0.3.7
+// color utility with methods for reading hex color values
+// and stored color presets.  Preset color arrays are made
+// out of three values: darkest/saturated/lightest a linear
+// progression is made from both ends towards the middle.
+// Color arrays are 9 segments in length stored in arrays. 
+// presets can be altered and new sets made through the
+// public method trispectrum:
+//	fly.color.rainbow = fly.color.trispectrum('#FF0000','#00FF00','0000FF')
+// common variables: 
+//	col is used for passed hex color values, ex. "#789ABC"
+// 	cola for color arrays, [r,g,b] ex [0,127,255]
+
+	var name = "infoCtrlr",
+		version = "0.3.7beta";
 
 	function limit(col){
 	    // limit col between 0 and 255
@@ -11,17 +26,18 @@ fly.color = (function() {
 	};
 	
 	function split(hexCol){
-	    // split a hex color to work with rgb
+	    // split a hex color into array [r,g,b]
 	    //assumes hex color w/ leading #
 	    var col = hexCol.slice(1);
 	    var col = parseInt(col,16);
 	    var r = (col >> 16);
-	    var g = ((col >> 8) & 0x00FF);
-	    var b = (col & 0x0000FF);
+	    var g = ((col >> 8) & 0xFF);
+	    var b = (col & 0xFF);
 	    return([r,g,b]);
 	};
 	
 	function splice(cola){
+		// takes a cola and returns the hex string value
 	    // cola is a color array [r,g,b], are all int
 	    var r = cola[0],
 	        g = cola[1],
@@ -37,8 +53,7 @@ fly.color = (function() {
 	
 	function mix(col1,col2,amt){
 	    // mixes 2 hex colors together, amt (0 to 1) determines ratio
-	    // amt = 0 returns col1, amt = 1 returns col2
-	    // amt defaults to .5
+	    // amt defaults to .5, mixes 50/50
 
 	    var amt = amt !== undefined ? amt : 0.5; 
 	    var col1a = split(col1),
@@ -51,7 +66,8 @@ fly.color = (function() {
 	
 	function spectrum(col1,col2,seg){
 		// takes two colors, returns array of seg sements
-	    // sent colors are first and last of the array
+		// each a hex color. sent colors are first and last 
+		// colors in the array
 		var seg = seg !== undefined ? seg : 5;
 	    if (seg < 3) {
 	        return [col1,col2];
@@ -64,12 +80,11 @@ fly.color = (function() {
 	    return spec;
 	};
 	
-	function flyspec(col1,col2,col3){
+	function trispectrum(col1,col2,col3){
 		// takes three hex colors and creates a 9 segment spectrum
 		// made for bringing saturated colors to light and dark
 		// standard use: (lightest, saturated, darkest)
 		// sent colors are first, middle, and last of the array
-		document.write('foo');
 		var lights = spectrum(col1,col2),
 			darks = spectrum(col2,col3);
 			// remove duplicate color in middle and merge
@@ -77,46 +92,47 @@ fly.color = (function() {
 			spec = lights.concat(darks);	
 			return spec;
 	};
-	
-	return {
-		limit : limit,
-		flySpec : flyspec
 		
+	return {
+		// LEGACY COLORS:
+		bkg: ["#00A9EB","#B0E5FF"], 
+		main: ["#FDE8A3","#8A8A39","#C6F063","#FF77CD"],
+		outln: ["#666666"],
+		selected: [],
+		info : { // colors used by infoCtrlr
+				title:	"#9BCAE1",	// sky blue
+				val:	"#89C234",	// apple green
+				btrue: "#66FF99",	// aqua
+				bfalse: "#3D9199", // dull aqua
+				event:	"#BC4500",
+				eventFiring: "#FF5E00",
+				version:"#8A8A39",	// pine green
+				info:	"#8A8A39",
+				screen: "#0D1927",	// skim milk
+				bar:	"black"
+		},
+			// preset color arrays
+		red : trispectrum('#400000','#FF0000','#FFC0C0'),
+		orange : trispectrum('#402900','#FFA500','#FFE8C0'),
+		yellow : trispectrum('#404000','#FFFF00','#FFFFC0'),
+		green : trispectrum('#004000','#00FF00','#C0FFC0'),
+		blue : trispectrum('#000040','#0000FF','#C0C0FF'),
+		purple: trispectrum('#400040','#800080','#FFC0FF'),
+		mono : trispectrum('#000000','#808080','#FFFFFF'),
+			// public methods 
+		mix : mix,
+		spectrum : spectrum,
+		trispectrum : trispectrum		
 	};
 	
 })();
 
-var lim = document.write("lim of 2000 = " + fly.color.limit(2000));
-var spectest = fly.color.flySpec('#000000','#00FF00','#FFFFFF');
-for (var i=0; i < spectest.length; i++) {
-	document.write(spectest[i] + '</br>');
-};
-
-var testTable = [
-'#000000',
-'#000002',
-'#000020',
-'#000200',
-'#002000',
-'#020000',
-'#020408',
-'#080808',
-'#888888',
-'#000000',
-'#FFFFFF'
-];
-
-var valTable = [-255,10,-1,0,1,10,255];
-
-
-// for (var i=0; i < testTable.length; i++) {
-//     for (var j=0; j < valTable.length; j++) {
-//         var col1 = testTable[i],
-//             col2 = testTable[i+1];
-//         var val = fly.color.mix(col1,col2,.5);
-//         document.write(testTable[i] + " + " + testTable[i+1] + " = " + val + "</br>");
-//     };
-// };
-
-
-​
+document.write("red = " + fly.color.red + '</br>');
+document.write("orange = " + fly.color.orange + '</br>');
+document.write("yellow = " + fly.color.yellow + '</br>');
+document.write("green = " + fly.color.green + '</br>');
+document.write("blue = " + fly.color.blue + '</br>');
+document.write("purple = " + fly.color.purple + '</br>');
+document.write("mono = " + fly.color.mono + '</br>');
+fly.color.red = fly.color.trispectrum('#AABBCC','#FFFFFF','#DD0000');
+document.write("red2 = " + fly.color.red + '</br>');
